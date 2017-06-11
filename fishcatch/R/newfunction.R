8a7262c022a4f5f8c97a8e196158b74f4a288b1f
ruchathakar <rucha@ucsb.edu>
2017-06-10 19:24:22 -0700

    Rucha's first commit

---
+++ fishcatch/R/newfunction.R
@@ -0,0 +1,14 @@
+#' documentation of this new function
+#' @author Rucha Thakar, Ignacia Rivera, Lina Barbosa
+#' @param
+#' @param
+#' @return
+#' @examples
+#'
+
+newfunction = function(someinput){
+  someinput = someinput+1
+  return (someinput)
+}
+
+
